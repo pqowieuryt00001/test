0976e0731bb7565028e455de2383f070f4d330f8
pqowieuryt00001 <30554776+pqowieuryt00001@users.noreply.github.com>
2017-07-31 22:52:28 +0900

    Create c.js

---
+++ c.js
@@ -0,0 +1,7 @@
+  (function() {
+      var elements = document.getElementsByClassName("mdMN17Ttl mdMN17Desc");
+      var rng = document.createRange();
+      rng.selectNodeContents(element);
+      window.getSelection().addRange(rng);
+      document.execCommand('copy');
+  })();
