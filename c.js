cd434a181fdc9d838716cf569a50867bd731fb12
pqowieuryt00001 <30554776+pqowieuryt00001@users.noreply.github.com>
2017-08-01 13:06:53 +0900

    Update c.js

--- c.js
+++ c.js
@@ -1,5 +1,5 @@
   (function() {
-      var elements = document.getElementsByClassName("mdMN17Ttl mdMN17Desc");
+      var elements = document.getElementsByClassName(".mdMN17Ttl .mdMN17Desc");
       var rng = document.createRange();
       rng.selectNodeContents(element);
       window.getSelection().addRange(rng);
